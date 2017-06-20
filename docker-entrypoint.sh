e3fb90ee6fb95d738d323680b15083fc1925d58d
mvuser <mvuser@mvmb114.lab.moviri.com>
2017-06-20 10:37:30 +0200

    first commit 2

---
+++ docker-entrypoint.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+set -e
+
+#we keep the cloudsuite entrypoint, it could come handy in a distributed setup
+
+echo Server IP
+ifconfig eth0 2>/dev/null | awk '/inet addr:/ {print $2}' | sed 's/addr://'
+
+if [ -z "$CASSANDRA_SEEDS" ]; then
+    NEED_INIT=1
+    echo Running Cassandra seed server.
+else
+    NEED_INIT=0
+    echo Running regular Cassandra server.
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1:0:1}" = '-' ]; then
+	set -- cassandra -f "$@"
+fi
+
+if [ "$1" = 'cassandra' ] || [ "$1" = 'bash' ]; then
+	: ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
+
+	: ${CASSANDRA_LISTEN_ADDRESS='auto'}
+	if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
+		CASSANDRA_LISTEN_ADDRESS="$(ip -o -4 addr list ens192 | sed -n 1p | awk '{print $4}' | cut -d/ -f1)"
+	fi
+
+	: ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
+
+	if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
+		CASSANDRA_BROADCAST_ADDRESS="$(ip -o -4 addr list ens192 | sed -n 1p | awk '{print $4}' | cut -d/ -f1)"
+	fi
+	: ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
+
+	if [ -n "${CASSANDRA_NAME:+1}" ]; then
+		: ${CASSANDRA_SEEDS:="cassandra"}
+	fi
+	: ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
+	
+	sed -ri 's/(- seeds:) "127.0.0.1"/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
+
+	for yaml in \
+		broadcast_address \
+		broadcast_rpc_address \
+		cluster_name \
+		endpoint_snitch \
+		listen_address \
+		num_tokens \
+		rpc_address \
+		start_rpc \
+	; do
+		var="CASSANDRA_${yaml^^}"
+		val="${!var}"
+		if [ "$val" ]; then
+			sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
+		fi
+	done
+
+	for rackdc in dc rack; do
+		var="CASSANDRA_${rackdc^^}"
+		val="${!var}"
+		if [ "$val" ]; then
+			sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
+		fi
+	done
+fi
+
+#exec "$@"
+cassandra
+sleep 5
+
+exit=0
+if [ $NEED_INIT -eq 1 ]; then
+    echo ======================================================
+    echo Create a usertable for the seed server
+    echo ======================================================
+    while [ $exit -eq 0 ]; do
+        if cqlsh localhost -f /setup_tables.txt ; then
+            exit=1
+        else
+            echo Cannot connect to the seed server. Trying again...
+        fi
+        sleep 5
+    done
+
+    printf "========\n--------------Keyspace usertable was created--------------\n========\n"
+else
+    echo "Cassandra seed server exists"
+fi
+
+while true; do
+    sleep 100;
+done
+
